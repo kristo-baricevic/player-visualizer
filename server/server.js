160a2dbd659d8861734c8ef2c8d2d58745bbdbe1
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-01-19 22:56:37 -0500

    moved cors

--- server/server.js
+++ server/server.js
@@ -4,18 +4,22 @@ const cors = require("cors");
 const path = require("path");
 const PORT = 8080;
 
-const songRouter = require("./controllers/songController");
-const songAnalyzerRouter = require("./controllers/songAnalyzerController");
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "public/images")));
-app.use("/music", cors(corsOptions), express.static(path.join(__dirname, "public/music")));
-
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+
+const songRouter = require("./controllers/songController");
+const songAnalyzerRouter = require("./controllers/songAnalyzerController");
+
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/music", cors(corsOptions), express.static(path.join(__dirname, "public/music")));
+
+
 
 app.use("/server-api", songRouter);
 app.use("/analyze", songAnalyzerRouter);
